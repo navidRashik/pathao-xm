9ab852b4671ba00e2bed9c0451d53c07b76489d4
navid.rashik <navid.anjum@evaly.com.bd>
2021-09-28 18:08:09 +0600

    implemented get_user api

--- apps/user/routers.py
+++ apps/user/routers.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from apps.user.views.users import post_user
+from apps.user.views.users import post_user, get_user
 user_router = APIRouter(
     prefix="/users",
     # tags=['users'],
@@ -8,7 +8,7 @@ user_router = APIRouter(
 
 
 user_router.add_api_route('/', post_user, methods=['POST'], status_code=201)
-
+user_router.add_api_route('/{id}', get_user, methods=['GET'])
 routers = [
     user_router,
 ]
--- apps/user/views/users.py
+++ apps/user/views/users.py
@@ -13,3 +13,17 @@ async def post_user(user: UserIn):
         first_name=user.first_name, last_name=user.last_name, hash_password=hash_password)
     last_record_id = await database.execute(query)
     return {"id": last_record_id}
+
+
+async def get_user(id: int):
+    query = users_table.select().where(users_table.c.id == id)
+    data = await database.fetch_one(query)
+
+    # values(
+    #     first_name=user.first_name, last_name=user.last_name, hash_password=hash_password)
+    first_name = data[1]
+    last_name = data[2]
+    return {
+        'id': data[0],
+        'name': f"{first_name} {last_name}"
+    }
